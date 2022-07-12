96ab850e7fdc6031a1991ac1326e565def3dcd55
Pierre-François Hau <pierrefrancois.hau@apside-groupe.com>
2022-07-12 11:00:05 +0200

    correction du problème

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,18 +1,18 @@
 import axios, { AxiosResponse } from "axios";
 
-export async function get(endpoint) {
+export async function get(endpoint: any) {
 
     return axios
         .get(process.env.VUE_APP_API_URL + endpoint)
 }
 
-export async function post(endpoint, datas) {
+export async function post(endpoint: any, datas: any) {
 
     type serviceResponse = {
         result: Boolean,
         response: AxiosResponse<any,any>;
     }
-    let res : serviceResponse;
+    let res! : serviceResponse;
 
     axios
         .post(process.env.VUE_APP_API_URL + endpoint, JSON.stringify(datas))
@@ -31,5 +31,5 @@ export async function post(endpoint, datas) {
             }
         );
 
-    return res;
+        return res;  
 }
